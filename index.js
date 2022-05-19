fd54591f7c54ec3c78c4458b49de8fd21e9aa553
Justin Voelkel <justin.voelkel@sourcetoad.com>
2022-05-19 13:09:03 -0400

    refactoring to use promises for the questions instead of nesting callbacks

--- index.js
+++ index.js
@@ -3,147 +3,149 @@ import readline from 'readline'
 import figures from 'figures'
 
 const rline = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 })
 
 const cellStates = Object.freeze({
-	dead: 0,
-	live: 1
+  dead: 0,
+  live: 1
 })
 
-function Cell(x, y){
-	this.x = x
-	this.y = y
-	let state = cellStates.dead
+function Cell (x, y) {
+  this.x = x
+  this.y = y
+  let state = cellStates.dead
 
-	this.setState = s => state = s
-	this.getState = () => state
+  this.setState = s => state = s
+  this.getState = () => state
 
-	this.outputCellState = () => {
-		let output = state ? figures.squareSmallFilled : ' '
-		process.stdout.write(ansiEscapes.cursorTo(this.x, this.y) + output)
-	}
+  this.outputCellState = () => {
+    const output = state ? figures.squareSmallFilled : ' '
+    process.stdout.write(ansiEscapes.cursorTo(this.x, this.y) + output)
+  }
 }
 
+function Board (x, y) {
+  this.height = x
+  this.width = y
+  this.cells = []
 
-function Board(x, y){
-	this.height = x
-	this.width = y
-	this.cells = []
+  const outputBoard = () => {
+    // console.log('output state of the board to stdout')
+    let x = 0
+    let y = 0
+    while (x < this.height) {
+      while (y < this.width) {
+        const cell = getCellByCoordinates(x, y)
+        cell.outputCellState()
+        y++
+      }
+      y = 0
+      x++
+    }
+  }
 
-	const outputBoard = () => {
-		//console.log('output state of the board to stdout')
-		let x = 0
-		let y = 0
-		while(x < this.height) {
-			while(y < this.width) {
-				const cell = getCellByCoordinates(x, y)
-				cell.outputCellState()
-				y++
-			}
-			y = 0
-			x++
-		}
-	}
+  const getCellByCoordinates = (x, y) => {
+    return this.cells.find(cell => cell.x === x && cell.y === y)
+  }
 
-	const getCellByCoordinates = (x, y) => {
-		return this.cells.find(cell => cell.x === x && cell.y === y)
-	}
+  const getCellNeighbors = (cell) => {
+    const east = getCellByCoordinates(cell.x + 1, cell.y)
+    const west = getCellByCoordinates(cell.x - 1, cell.y)
+    const north = getCellByCoordinates(cell.x, cell.y + 1)
+    const south = getCellByCoordinates(cell.x, cell.y - 1)
 
-	const getCellNeighbors = (cell) => {
-		let east = getCellByCoordinates(cell.x + 1, cell.y)
-		let west = getCellByCoordinates(cell.x - 1, cell.y)
-		let north = getCellByCoordinates(cell.x, cell.y + 1)
-		let south = getCellByCoordinates(cell.x, cell.y - 1)
+    const northWest = north ? getCellByCoordinates(north.x - 1, north.y) : null
+    const northEast = north ? getCellByCoordinates(north.x + 1, north.y) : null
+    const southWest = south ? getCellByCoordinates(south.x - 1, south.y) : null
+    const southEast = south ? getCellByCoordinates(south.x + 1, south.y) : null
 
-		let northWest = north ? getCellByCoordinates(north.x - 1, north.y) : null
-		let northEast = north ? getCellByCoordinates(north.x + 1, north.y) : null
-		let southWest = south ? getCellByCoordinates(south.x - 1, south.y) : null
-		let southEast = south ? getCellByCoordinates(south.x + 1, south.y) : null
+    return {
+      north,
+      east,
+      south,
+      west,
+      northEast,
+      southEast,
+      northWest,
+      southWest
+    }
+  }
 
+  this.init = () => {
+    let x = 0
+    let y = 0
 
-		return {
-			north,
-			east,
-			south,
-			west,
-			northEast,
-			southEast,
-			northWest,
-			southWest
-		}
-	}
+    // randomly seed some live cells
+    const randomizer = () => Math.random() * (10 - 0) + 0
 
-	this.init = () => {
-		let x = 0
-		let y = 0
+    while (x < this.height) {
+      while (y < this.width) {
+        const cell = new Cell(x, y)
+        if (randomizer() < 3) {
+          cell.setState(cellStates.live)
+        }
+        this.cells.push(cell)
+        y++
+      }
+      y = 0
+      x++
+    }
+  }
 
-		// randomly seed some live cells
-                let randomizer = () => Math.random() * (10 - 0) + 0
+  this.tick = () => {
+    // run rules against all cells
+    // output the board
+    this.cells.forEach(cell => {
+      const neighbors = getCellNeighbors(cell)
+      const neighborStates = Object.values(neighbors)
+        .map(neighbor => neighbor ? neighbor.getState() : null)
+      const liveNeighbors = neighborStates.reduce((acc, state) => acc + state, 0)
 
-		while(x < this.height){
-			while(y < this.width){
-				let cell = new Cell(x, y)
-				if(randomizer() < 3) {
-					cell.setState(cellStates.live)
-				}
-				this.cells.push(cell)
-				y++
-			}
-			y = 0
-			x++
-		}
-	}
+      // live rules
+      if (cell.getState()) {
+        if (liveNeighbors < 2 || liveNeighbors > 3) {
+          cell.setState(cellStates.dead)
+        }
+        if (liveNeighbors === 2 || liveNeighbors === 3) {
+          cell.setState(cellStates.live)
+        }
+      }
 
-	this.tick = () => {
-		// run rules against all cells
-		// output the board
-		this.cells.forEach(cell => {
-			const neighbors = getCellNeighbors(cell)
-			const neighborStates = Object.values(neighbors)
-				.map(neighbor => neighbor ? neighbor.getState() : null)
-			const liveNeighbors = neighborStates.reduce((acc, state) => acc + state, 0)
-			
-			// live rules
-			if(cell.getState()) {
-				if (liveNeighbors < 2 || liveNeighbors > 3) {
-					cell.setState(cellStates.dead)
-				}
-				if (liveNeighbors === 2 || liveNeighbors ===3) {
-					cell.setState(cellStates.live)
-				}
-			}
-
-			// dead rules
-			if(!cell.getState()) {
-				if (liveNeighbors === 3) {
-					cell.setState(cellStates.live)
-				}
-			}
-		})
-		outputBoard()
-	}
-
+      // dead rules
+      if (!cell.getState()) {
+        if (liveNeighbors === 3) {
+          cell.setState(cellStates.live)
+        }
+      }
+    })
+    outputBoard()
+  }
 }
 
+const askQuestion = question => {
+  return new Promise((resolve, reject) => {
+    rline.question(question, response => {
+      resolve(response)
+    })
+  })
+}
 
-const initGame = () => {
-	rline.question('board height?', height => {
-		rline.question('board width?', width => {
-			rline.question('how many generations?', generations => {
-				const board = new Board(height, width)
-				board.init()
-				
-				let counter = 0
-				while(counter <= generations){
-					board.tick()
-					counter++
-				}
-				rline.close()
-			})
-		})
-	})
+const initGame = async () => {
+  const height = await askQuestion('board height? ')
+  const width = await askQuestion('board width? ')
+  const generations = await askQuestion('number of generations? ')
+  rline.close()
+
+  const board = new Board(height, width)
+  board.init()
+
+  let generationCounter = 0
+  while (generationCounter <= generations) {
+    board.tick()
+    generationCounter++
+  }
 }
 
 initGame()
